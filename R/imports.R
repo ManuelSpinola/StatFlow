aad0216d8600110d757cf0e81ca3b24eb70ae71e
Manuel Spínola <mspinola10@gmail.com>
2026-05-19 17:26:28 -0600

    fix: archivo imports.R con todos los @import necesarios

---
+++ R/imports.R
@@ -0,0 +1,12 @@
+#' @import shiny
+#' @import bslib
+#' @import bsicons
+#' @import shinyjs
+#' @import ggplot2
+#' @import dplyr
+#' @import tidyr
+#' @import purrr
+#' @importFrom DT DTOutput renderDT datatable
+#' @importFrom readr read_delim
+#' @importFrom readxl read_excel
+NULL
